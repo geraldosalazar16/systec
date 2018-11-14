53ba7557d95fb0fea48e9cbddbd4aefc922d4140
geraldosalazar16 <geraldosalazar16@gmail.com>
2018-11-14 15:45:34 +0100

    cambios locales

--- routes/loginSmartsheet.js
+++ routes/loginSmartsheet.js
@@ -3,11 +3,25 @@ var router = express.Router();
 
 var userModel = require('../models/users.js');
 
-const authorizationUrl = 'https://app.smartsheet.com/b/authorize?response_type=code&client_id=14g7rriwn93nerf1kud&scope=WRITE_SHEETS%20READ_SHEETS&state=ExampleString';
+const qs = require('querystring');
+
+const authorizationUri = authorizeURL({
+    response_type: 'code',
+    client_id: '14g7rriwn93nerf1kud',
+    redirect_uri: 'http://localhost:3000/loginSmartsheet',
+    scope: 'CREATE_SHEETS WRITE_SHEETS',
+});
+
+function authorizeURL(params) {
+    const authUrl = 'https://app.smartsheet.com/b/authorize';
+    return `${authUrl}?${qs.stringify(params)}`;
+}
+
+//const authorizationUrl = 'https://app.smartsheet.com/b/authorize?response_type=code&client_id=14g7rriwn93nerf1kud&scope=WRITE_SHEETS%20READ_SHEETS&state=ExampleString';
 
 router.post('/',function(req, res, next) {
-    console.log('Your authorization url: ', authorizationUrl);
-    res.send(authorizationUrl);
+    console.log('Your authorization url: ', authorizationUri);
+    res.send(authorizationUri);
 });
 
 router.get('/',function(req, res, next) {
@@ -21,7 +35,8 @@ router.get('/',function(req, res, next) {
            client_id: '14g7rriwn93nerf1kud',
            code: authCode,
            hash: generated_hash
-       }
+       },
+       contentType: 'application/x-www-form-urlencoded'
    };
    var smartsheet = require('smartsheet');
    smartsheet.tokens.getAccessToken(options, processToken)
